4f3808615d8ada559acd81b1fbaad3af6d73b517
Ruud van Asseldonk <ruud@chorus.one>
2024-05-20 11:42:37 +0200

    Enable basic webserver

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use tiny_http::{Server, Response};
+
 fn main() {
-    println!("Hello, world!");
+    // TODO: Enable override.
+    let server = Server::http("127.0.0.1:5591").unwrap();
+    println!("Listening on http://127.0.0.1:5591/");
+
+    for request in server.incoming_requests() {
+        println!("received request! method: {:?}, url: {:?}, headers: {:#?}",
+            request.method(),
+            request.url(),
+            request.headers()
+        );
+
+        let response = Response::from_string("hello world");
+        let err = request.respond(response);
+        if let Err(err) = err {
+            println!("Error: {err:?}");
+        }
+    }
 }
