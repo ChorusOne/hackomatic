4dc7b6653dc81ef9c997281dacb7f5293e477fc0
Ruud van Asseldonk <ruud@chorus.one>
2024-05-20 12:30:36 +0200

    Extract the user from the request

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,73 @@
-use tiny_http::{Server, Response};
+use std::io::Cursor;
+use std::str::FromStr;
+
+use tiny_http::{HeaderField, Request, Server};
+
+type Response = tiny_http::Response<Cursor<Vec<u8>>>;
+
+struct Config {
+    is_debug: bool,
+    listen: String,
+}
+
+fn parse_cli() -> Config {
+    let mut result = Config {
+        is_debug: false,
+        listen: "127.0.0.1:5591".to_string(),
+    };
+
+    let mut args = std::env::args();
+
+    // Skip the program name.
+    args.next();
+
+    for arg in args {
+        match arg.as_ref() {
+            "--debug" => result.is_debug = true,
+            _ => result.listen = arg,
+        }
+    }
+
+    result
+}
+
+fn handle_request(config: &Config, request: &mut Request) -> Response {
+    println!(
+        "received request! method: {:?}, url: {:?}, headers: {:?}",
+        request.method(),
+        request.url(),
+        request.headers()
+    );
+
+    // Figure out who the user is. In debug mode we fall back to a default.
+    let header_x_email = HeaderField::from_str("X-Email").unwrap();
+    let mut email = None;
+    for header in request.headers() {
+        if header.field == header_x_email {
+            email = Some(header.value.as_str());
+        }
+    }
+    let email = match email {
+        Some(email) => email,
+        None if config.is_debug => "debug@example.com",
+        _ => return Response::from_string("Missing authentication header."),
+    };
+
+    let body = format!("Hello {email}.");
+    Response::from_string(body)
+}
 
 fn main() {
-    // TODO: Enable override.
-    let server = Server::http("127.0.0.1:5591").unwrap();
-    println!("Listening on http://127.0.0.1:5591/");
+    let config = parse_cli();
 
-    for request in server.incoming_requests() {
-        println!("received request! method: {:?}, url: {:?}, headers: {:#?}",
-            request.method(),
-            request.url(),
-            request.headers()
-        );
+    let server = Server::http(&config.listen).unwrap();
+    println!("Listening on http://{}/", config.listen);
 
-        let response = Response::from_string("hello world");
-        let err = request.respond(response);
-        if let Err(err) = err {
-            println!("Error: {err:?}");
+    for mut request in server.incoming_requests() {
+        let response = handle_request(&config, &mut request);
+        match request.respond(response) {
+            Err(err) => println!("Error: {err:?}"),
+            Ok(()) => continue,
         }
     }
 }
